f0e77cf454ea0a22e92e9a5c6242a516ad50a4d7
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-02-17 11:25:45 +0900

    error handle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 export default function Home() {
   return (
     <div>
       <div className="py-5 text-center">
         <div className="block text-xl font-bold text-gray-800 dark:text-white"></div>
         私たちについて
+        <Image src="/chara.png" height={400} width={300} alt="tosi"></Image>
       </div>
       <span className="text-sm text-gray-700 dark:text-gray-200">Horizon</span>
     </div>
