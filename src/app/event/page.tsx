d8fb07d1c9f7d35a1fa7b2dffe03704374a81ec3
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-03-01 18:49:14 +0900

    errohandle

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <div>
       <div className="py-5 text-center">
         <div className="block text-xl font-bold text-gray-800 dark:text-white"></div>
-
         <Button disabled>
           <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
           Please wait
