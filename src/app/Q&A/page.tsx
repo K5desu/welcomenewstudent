e8950146057a76c7dabb1c471c4739b121501b4b
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-03-02 22:13:45 +0900

    add Q&A

--- src/app/Q&A/page.tsx
+++ src/app/Q&A/page.tsx
@@ -3,14 +3,19 @@ import { ReloadIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const question: string | never[] = [];
   return (
     <div>
       <div className="py-5 text-center">
         <div className="block text-xl font-bold text-gray-800 dark:text-white"></div>
-        <Button disabled>
-          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-          Coming soon
-        </Button>
+        {question.length > 0 ? (
+          <div>{question}</div>
+        ) : (
+          <Button disabled>
+            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+            Coming soon
+          </Button>
+        )}
       </div>
     </div>
   );
