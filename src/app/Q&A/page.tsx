bb74f158a41d14bd8e34d964d166d1e8deb260d1
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-03-06 11:21:41 +0900

    add

--- src/app/Q&A/page.tsx
+++ src/app/Q&A/page.tsx
@@ -1,6 +1,5 @@
-import { ReloadIcon } from "@radix-ui/react-icons";
+import Qa from "@/components/my/Qa";
 
-import { Button } from "@/components/ui/button";
 import Coming from "@/components/ui/comingsoon";
 
 export default function Home() {
@@ -16,139 +15,111 @@ export default function Home() {
                 最新の質問
               </h1>
 
-              <div className="mt-12 space-y-8">
-                <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
-                  <button className="flex items-center justify-between w-full p-8">
-                    <h1 className="font-semibold text-gray-700 dark:text-white text-center">
-                      男女比は ?
-                    </h1>
+              <Qa
+                question="学校の雰囲気はどんな感じですか？"
+                answer="学校の雰囲気はとても良いです。"
+              />
 
-                    <span className="text-gray-400 bg-gray-200 rounded-full">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M18 12H6"
-                        />
-                      </svg>
-                    </span>
-                  </button>
+              <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
+                <button className="flex items-center justify-between w-full p-8">
+                  <h1 className="font-semibold text-gray-700 dark:text-white">
+                    <Coming />
+                  </h1>
 
-                  <hr className="border-gray-200 dark:border-gray-700" />
+                  <span className="text-white bg-blue-500 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </span>
+                </button>
+              </div>
 
-                  <p className="p-8 text-sm text-gray-500 dark:text-gray-300">
-                    男女比は7:3くらいで男が多いです。先輩は三回生が最高学年です。
-                  </p>
-                </div>
+              <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
+                <button className="flex items-center justify-between w-full p-8">
+                  <h1 className="font-semibold text-gray-700 dark:text-white">
+                    <Coming />
+                  </h1>
 
-                <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
-                  <button className="flex items-center justify-between w-full p-8">
-                    <h1 className="font-semibold text-gray-700 dark:text-white">
-                      <Coming />
-                    </h1>
+                  <span className="text-white bg-blue-500 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </span>
+                </button>
+              </div>
 
-                    <span className="text-white bg-blue-500 rounded-full">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                        />
-                      </svg>
-                    </span>
-                  </button>
-                </div>
+              <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
+                <button className="flex items-center justify-between w-full p-8">
+                  <h1 className="font-semibold text-gray-700 dark:text-white">
+                    {" "}
+                    <Coming />
+                  </h1>
 
-                <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
-                  <button className="flex items-center justify-between w-full p-8">
-                    <h1 className="font-semibold text-gray-700 dark:text-white">
-                      <Coming />
-                    </h1>
+                  <span className="text-white bg-blue-500 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </span>
+                </button>
+              </div>
 
-                    <span className="text-white bg-blue-500 rounded-full">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                        />
-                      </svg>
-                    </span>
-                  </button>
-                </div>
+              <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
+                <button className="flex items-center justify-between w-full p-8">
+                  <h1 className="font-semibold text-gray-700 dark:text-white">
+                    {" "}
+                    <Coming />
+                  </h1>
 
-                <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
-                  <button className="flex items-center justify-between w-full p-8">
-                    <h1 className="font-semibold text-gray-700 dark:text-white">
-                      {" "}
-                      <Coming />
-                    </h1>
-
-                    <span className="text-white bg-blue-500 rounded-full">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                        />
-                      </svg>
-                    </span>
-                  </button>
-                </div>
-
-                <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
-                  <button className="flex items-center justify-between w-full p-8">
-                    <h1 className="font-semibold text-gray-700 dark:text-white">
-                      {" "}
-                      <Coming />
-                    </h1>
-
-                    <span className="text-white bg-blue-500 rounded-full">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                        />
-                      </svg>
-                    </span>
-                  </button>
-                </div>
+                  <span className="text-white bg-blue-500 rounded-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </span>
+                </button>
               </div>
             </div>
           </section>
