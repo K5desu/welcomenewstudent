8e33a5b531e71adfd4ab73c8fd3a6ebb7823ae80
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-03-06 17:34:17 +0900

    aa

--- src/components/my/Qa.tsx
+++ src/components/my/Qa.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 export default function Qa({ question, answer }: CardProps) {
   const [showAnswer, setShowAnswer] = useState(false);
   return (
-    <div className="mt-12 space-y-8">
+    <div className="border-2 border-gray-100 rounded-lg dark:border-gray-700">
       <button
         className="flex items-center justify-between w-full p-8"
         onClick={() => {
@@ -21,7 +21,7 @@ export default function Qa({ question, answer }: CardProps) {
           {question}
         </h1>
         {showAnswer ? (
-          <span className="text-gray-400 bg-gray-200 rounded-full">
+          <span className="text-white bg-blue-500 rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-6 h-6"
@@ -30,10 +30,10 @@ export default function Qa({ question, answer }: CardProps) {
               stroke="currentColor"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M18 12H6"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
               />
             </svg>
           </span>
@@ -56,22 +56,25 @@ export default function Qa({ question, answer }: CardProps) {
           </span>
         )}
       </button>
+      {showAnswer && (
+        <div>
+          <hr
+            className={clsx(
+              "border-gray-200 dark:border-gray-700 transition-transform duration-300 ease-out transform translate-y-[-100%] opacity-0",
+              { "translate-y-[0%] opacity-100": showAnswer }
+            )}
+          />
 
-      <hr
-        className={clsx(
-          "border-gray-200 dark:border-gray-700 transition-transform duration-300 ease-out transform translate-y-[-100%] opacity-0",
-          { "translate-y-[0%] opacity-100": showAnswer }
-        )}
-      />
-
-      <p
-        className={clsx(
-          "p-8 text-sm text-gray-500 dark:text-gray-300  transition-transform duration-300 ease-out transform translate-y-[-100%] opacity-0",
-          { "translate-y-[0%] opacity-100": showAnswer }
-        )}
-      >
-        {answer}
-      </p>
+          <p
+            className={clsx(
+              "p-8 text-sm text-gray-500 dark:text-gray-300  transition-transform duration-300 ease-out transform translate-y-[-100%] opacity-0",
+              { "translate-y-[0%] opacity-100": showAnswer }
+            )}
+          >
+            {answer}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
