5968a354a0547d8d826044d7b44723997c0ecc85
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-03-25 11:42:10 +0900

    addadmin

--- src/lib/Check.ts
+++ src/lib/Check.ts
@@ -2,7 +2,9 @@ export default function Check(email: string | null | undefined) {
   if (
     email !== "dekinousyonn@gmail.com" &&
     email !== "y230190@mail.ryukoku.ac.jp" &&
-    email !== "ryukokuhorizon.member@gmail.com"
+    email !== "ryukokuhorizon.member@gmail.com" &&
+    email !== "y220023@mail.ryukoku.ac.jp" &&
+    email !== "e220012@mail.ryukoku.ac.jp"
   )
     return true;
 }
