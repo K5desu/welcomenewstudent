b19924f51376d431210871b1afbaedb35c7a3379
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-03-25 14:49:12 +0900

    deletemember

--- src/lib/Check.ts
+++ src/lib/Check.ts
@@ -2,7 +2,6 @@ export default function Check(email: string | null | undefined) {
   const allowedEmails = [
     "dekinousyonn@gmail.com",
     "y230190@mail.ryukoku.ac.jp",
-    "ryukokuhorizon.member@gmail.com",
     "y220023@mail.ryukoku.ac.jp",
     "e220012@mail.ryukoku.ac.jp",
   ];
