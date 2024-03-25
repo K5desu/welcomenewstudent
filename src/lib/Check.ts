dedac3b0febfeff77c082b94680a7d49d12e2297
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-03-25 11:47:59 +0900

    add

--- src/lib/Check.ts
+++ src/lib/Check.ts
@@ -1,10 +1,11 @@
 export default function Check(email: string | null | undefined) {
-  if (
-    email !== "dekinousyonn@gmail.com" &&
-    email !== "y230190@mail.ryukoku.ac.jp" &&
-    email !== "ryukokuhorizon.member@gmail.com" &&
-    email !== "y220023@mail.ryukoku.ac.jp" &&
-    email !== "e220012@mail.ryukoku.ac.jp"
-  )
-    return true;
+  const allowedEmails = [
+    "dekinousyonn@gmail.com",
+    "y230190@mail.ryukoku.ac.jp",
+    "ryukokuhorizon.member@gmail.com",
+    "y220023@mail.ryukoku.ac.jp",
+    "e220012@mail.ryukoku.ac.jp",
+  ];
+
+  return !allowedEmails.some((allowedEmail) => allowedEmail === email);
 }
